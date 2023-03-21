17e28e62802a5aaeeb6cc44d0da23a374f145e1f
Djmysteek <118613522+Djmysteek@users.noreply.github.com>
2023-03-21 00:29:09 -0400

    Delete .history directory

--- .history/statefile_20230320233946.tf
+++
@@ -1,8 +0,0 @@
-resource "google_storage_bucket" "Terraform-Project" {
-  name          = "terraform-project-bucket"
-  location      = "US"
-  force_destroy = true
-  public_access_prevention = "enforced"
-
-  uniform_bucket_level_access = true
-}
--- .history/statefile_20230320234915.tf
+++
@@ -1,9 +0,0 @@
-
-resource "google_storage_bucket" "terraform_project" {
-  name          = "mystee_terraform-project-bucket"
-  location      = "US"
-  force_destroy = true
-  public_access_prevention = "enforced"
-
-  uniform_bucket_level_access = true
-}
